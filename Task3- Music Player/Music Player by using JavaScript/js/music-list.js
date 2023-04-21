9264411a1cf9e47bbf2e7b41a70fb95a6b518db3
KastureTanvi <95160600+KastureTanvi@users.noreply.github.com>
2023-04-21 16:44:30 +0530

    Add files via upload

---
+++ Task3- Music Player/Music Player by using JavaScript/js/music-list.js
@@ -0,0 +1,42 @@
+
+
+let allMusic = [
+  {
+    name: "Double take ",
+    artist: "Dhruv",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Колыбельная",
+    name: "Rauf & Faik",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "shinunoga e-wa",
+    artist: "Fujii Kaze",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Shape of You",
+    artist: "Ed Sheeran",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-5",
+    src: "music-5"
+  },
+
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+
+];
